201af613c6f8f6783bfc50f14e4bc0366620b9a3
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-16 23:00:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app_mode = st.sidebar.selectbox('Secciones',['Home','Ejercicio 1','Ejercicio 2',
 
 if app_mode == 'Home':
   st.title ('Proyecto N°1 de la Especialización de Python')
+  st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+  st.markdown()
   st.image('Python_logo.png')
   st.image('DMC.png')
   st.write('Estudiante: Miguel Angel Limaquispe Huaman')
@@ -11,5 +13,6 @@ if app_mode == 'Home':
   st.write('Información General: Ingeniero de Sistemas con experiencia en Bussines Intelligence en Sectores de Banca, Telecomunicaciones, Consumo Masivo y Farmaceútico.')
   st.write('Año: 34 años')
   st.write('Descripción: Proyecto enfocado en el desarrollo de una Aplicación con Streamlit')
+ 
   
   
