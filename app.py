58c175811d2557b34c2f8293eb3fd38ad850a096
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-16 19:36:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 app_mode = st.sidebar.selectbox('Secciones',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
 
 if app_mode == 'Home':
-  st.title ('Proeycto N°1 de la Especializacipon de Python')
+  st.title ('Proyecto N°1 de la Especialización de Python')
