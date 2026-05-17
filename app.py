093d4589bf1c476b22d0399cfc69e4f63c3105f2
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-16 19:27:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 import streamlit as st
-st.sidebar.selectbox('Secciones',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
+st.selectbox('Secciones',['Home','Ejercicio 1','Ejercicio 2','Ejercicio 3','Ejercicio 4'])
