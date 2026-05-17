541c6116a24d6d3dd7578c537a46b177baf49eec
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-17 02:09:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,19 @@ if app_mode == 'Home':
     Descripción: Proyecto enfocado en el desarrollo de una Aplicación con Streamlit
     """
     )
+ 
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors] and :blue-background[highlight] text.''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
   
  
   
