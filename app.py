c798dbde956929d9bfca53724ccc9a837c4f5f28
miguelangel120917 <53247609+miguelangel120917@users.noreply.github.com>
2026-05-26 19:13:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -152,9 +152,8 @@ elif app_mode == 'Ejercicio 2':
     else:
         st.info("Aún no hay registros.")
 elif app_mode == 'Ejercicio 3':
-  # --- Configuración de la página ---
-    st.set_page_config(page_title="Ejercicio 3 - Cálculo")
-  # 1. Inicializar el histórico en el session_state
+  
+# 1. Inicializar el histórico en el session_state
   
 if 'historico_resultados' not in st.session_state:
       st.session_state.historico_resultados = []
